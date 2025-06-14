9abdbdccc1288248958d8836ce43bc17402dc1e7
Mincai Wang <mincaiw@yonsei.com>
2025-06-15 07:14:33 +0900

    New update

--- Circle_of_Life.py
+++ Circle_of_Life.py
@@ -1,197 +1,188 @@
 import random
+import os
+import sys
 
+# Parameters
 GRID_SIZE = 50
 INITIAL_ZEBRAS = 20
 INITIAL_LIONS = 5
 YEARS = 20
 
-EMPTY = '.'
-ZEBRA = 'Z'
-LION = 'L'
+class Cell:
+    def __init__(self):
+        self.grass = True
+        self.regrow_timer = 0
+
+    def eaten(self):
+        self.grass = False
+        self.regrow_timer = 1
+
+    def step(self):
+        if not self.grass:
+            self.regrow_timer -= 1
+            if self.regrow_timer <= 0:
+                self.grass = True
 
 class Animal:
     def __init__(self, x, y):
         self.x = x
         self.y = y
         self.age = 0
-        self.hungry_years = 0
+        self.hungry = 0
         self.alive = True
 
-    def get_neighbors(self, grid, target):
-        neighbors = []
+    def neighbors(self):
         for dx, dy in [(-1,0),(1,0),(0,-1),(0,1)]:
             nx, ny = self.x + dx, self.y + dy
             if 0 <= nx < GRID_SIZE and 0 <= ny < GRID_SIZE:
-                if grid[nx][ny] == target:
-                    neighbors.append((nx, ny))
-        return neighbors
+                yield nx, ny
+
+    def move(self, ecosys):
+        raise NotImplementedError
+
+    def step(self, ecosys, newborns):
+        if not self.alive:
+            return
+        self.age += 1
+        ate = self.move(ecosys)
+        self.hungry = 0 if ate else self.hungry + 1
+        if self.dead():
+            self.alive = False
+            return
+        baby = self.breed()
+        if baby:
+            newborns.append(baby)
+
+    def dead(self):
+        raise NotImplementedError
+
+    def breed(self):
+        raise NotImplementedError
 
 class Zebra(Animal):
-    def move(self, grid):
-        neighbors = self.get_neighbors(grid, EMPTY)
-        if neighbors:
-            nx, ny = random.choice(neighbors)
-            grid[self.x][self.y] = EMPTY
-            self.x, self.y = nx, ny
-            grid[nx][ny] = ZEBRA
+    def move(self, ecosys):
+        grass_cells = [(nx,ny) for nx,ny in self.neighbors()
+                       if ecosys.cells[ny][nx].grass and not ecosys.occupied(nx,ny)]
+        if grass_cells:
+            nx, ny = random.choice(grass_cells)
+            ecosys.move(self, nx, ny)
+            ecosys.cells[ny][nx].eaten()
+            return True
+        empties = [(nx,ny) for nx,ny in self.neighbors() if not ecosys.occupied(nx,ny)]
+        if empties:
+            nx, ny = random.choice(empties)
+            ecosys.move(self, nx, ny)
+        return False
 
-    def graze(self, grass_map):
-        if grass_map[self.x][self.y]:
-            self.hungry_years = 0
-            grass_map[self.x][self.y] = False
-        else:
-            self.hungry_years += 1
+    def dead(self):
+        return self.hungry >= 3
 
-    def check_death(self):
-        if self.hungry_years >= 3:
-            self.alive = False
-
-    def can_breed(self):
-        return self.age >= 3
-
-class Lion(Animal):
-    def move(self, grid):
-        zebra_neighbors = self.get_neighbors(grid, ZEBRA)
-        if zebra_neighbors:
-            nx, ny = random.choice(zebra_neighbors)
-            grid[self.x][self.y] = EMPTY
-            self.x, self.y = nx, ny
-            grid[nx][ny] = LION
-            return (nx, ny)
-        neighbors = self.get_neighbors(grid, EMPTY)
-        if neighbors:
-            nx, ny = random.choice(neighbors)
-            grid[self.x][self.y] = EMPTY
-            self.x, self.y = nx, ny
-            grid[nx][ny] = LION
+    def breed(self):
+        if self.age >= 3:
+            self.age = 0
+            return Zebra(self.x, self.y)
         return None
 
-    def hunt(self, eaten_pos):
-        if eaten_pos:
-            self.hungry_years = 0
-        else:
-            self.hungry_years += 1
+class Lion(Animal):
+    def move(self, ecosys):
+        prey = [(nx,ny) for nx,ny in self.neighbors()
+                if any(isinstance(a, Zebra) for a in ecosys.animals_at(nx,ny))]
+        if prey:
+            nx, ny = random.choice(prey)
+            victim = next(a for a in ecosys.animals_at(nx,ny) if isinstance(a, Zebra))
+            victim.alive = False
+            ecosys.move(self, nx, ny)
+            return True
+        empties = [(nx,ny) for nx,ny in self.neighbors() if not ecosys.occupied(nx,ny)]
+        if empties:
+            nx, ny = random.choice(empties)
+            ecosys.move(self, nx, ny)
+        return False
 
-    def check_death(self):
-        if self.hungry_years >= 5:
-            self.alive = False
+    def dead(self):
+        return self.hungry >= 5
 
-    def can_breed(self):
-        return self.age >= 5
+    def breed(self):
+        if self.age >= 5:
+            self.age = 0
+            return Lion(self.x, self.y)
+        return None
 
 class Ecosystem:
     def __init__(self):
-        self.grid = [[EMPTY for _ in range(GRID_SIZE)] for _ in range(GRID_SIZE)]
-        self.grass_map = [[True for _ in range(GRID_SIZE)] for _ in range(GRID_SIZE)]
+        self.cells = [[Cell() for _ in range(GRID_SIZE)] for _ in range(GRID_SIZE)]
         self.zebras = []
         self.lions = []
-        self.timestep = 0
-        self.init_animals()
+        self._populate()
 
-    def init_animals(self):
-        positions = set()
-        while len(self.zebras) < INITIAL_ZEBRAS:
-            x, y = random.randrange(GRID_SIZE), random.randrange(GRID_SIZE)
-            if (x, y) not in positions:
-                self.zebras.append(Zebra(x, y))
-                self.grid[x][y] = ZEBRA
-                positions.add((x, y))
-        while len(self.lions) < INITIAL_LIONS:
-            x, y = random.randrange(GRID_SIZE), random.randrange(GRID_SIZE)
-            if (x, y) not in positions:
-                self.lions.append(Lion(x, y))
-                self.grid[x][y] = LION
-                positions.add((x, y))
+    @property
+    def animals(self):
+        return [*self.zebras, *self.lions]
 
-    def year_pass(self):
-        self.timestep += 1
-        for z in self.zebras:
-            if z.alive:
-                z.age += 1
-        for l in self.lions:
-            if l.alive:
-                l.age += 1
+    def _populate(self):
+        coords = [(x,y) for x in range(GRID_SIZE) for y in range(GRID_SIZE)]
+        random.shuffle(coords)
+        for _ in range(INITIAL_ZEBRAS):
+            x,y = coords.pop()
+            self.zebras.append(Zebra(x,y))
+        for _ in range(INITIAL_LIONS):
+            x,y = coords.pop()
+            self.lions.append(Lion(x,y))
 
-        for l in self.lions:
-            if l.alive:
-                eaten_pos = l.move(self.grid)
-                l.hunt(eaten_pos)
-                if eaten_pos:
-                    for z in self.zebras:
-                        if z.alive and (z.x, z.y) == eaten_pos:
-                            z.alive = False
-                            break
+    def occupied(self, x, y):
+        return any(a.alive and a.x==x and a.y==y for a in self.animals)
 
-        for z in self.zebras:
-            if z.alive:
-                z.move(self.grid)
+    def animals_at(self, x, y):
+        return [a for a in self.animals if a.alive and a.x==x and a.y==y]
 
-        for z in self.zebras:
-            if z.alive:
-                z.graze(self.grass_map)
+    def move(self, animal, nx, ny):
+        animal.x, animal.y = nx, ny
 
-        for z in self.zebras:
-            if z.alive:
-                z.check_death()
-        for l in self.lions:
-            if l.alive:
-                l.check_death()
+    def step(self):
+        newborns = []
+        for a in list(self.animals):
+            a.step(self, newborns)
+        self.zebras = [z for z in self.zebras if z.alive]
+        self.lions = [l for l in self.lions if l.alive]
+        for baby in newborns:
+            if isinstance(baby, Zebra): self.zebras.append(baby)
+            else: self.lions.append(baby)
+        for row in self.cells:
+            for cell in row:
+                cell.step()
 
-        new_zebras = []
-        for z in self.zebras:
-            if z.alive and z.can_breed():
-                neighbors = z.get_neighbors(self.grid, EMPTY)
-                if neighbors:
-                    nx, ny = random.choice(neighbors)
-                    new_zebras.append(Zebra(nx, ny))
-                    self.grid[nx][ny] = ZEBRA
-        self.zebras.extend(new_zebras)
+    def stats(self):
+        return len(self.zebras), len(self.lions)
 
-        new_lions = []
-        for l in self.lions:
-            if l.alive and l.can_breed():
-                neighbors = l.get_neighbors(self.grid, EMPTY)
-                if neighbors:
-                    nx, ny = random.choice(neighbors)
-                    new_lions.append(Lion(nx, ny))
-                    self.grid[nx][ny] = LION
-        self.lions.extend(new_lions)
-
-        for i in range(GRID_SIZE):
-            for j in range(GRID_SIZE):
-                self.grass_map[i][j] = True
-
-    def stat(self):
-        zebras_alive = sum(1 for z in self.zebras if z.alive)
-        lions_alive = sum(1 for l in self.lions if l.alive)
-        return zebras_alive, lions_alive
-
-    def display(self, area=10):
-        print(f'Clock: {self.timestep}')
-        # 列号部分：每个两格+空格
-        col_head = '   ' + ''.join(f'{coord:2} ' for coord in range(area))
-        print(col_head)
-        show_grid = [[EMPTY for _ in range(area)] for _ in range(area)]
-        for z in self.zebras:
-            if z.alive and z.x < area and z.y < area:
-                show_grid[z.x][z.y] = ZEBRA
-        for l in self.lions:
-            if l.alive and l.x < area and l.y < area:
-                show_grid[l.x][l.y] = LION
-        for row in range(area):
-            line = ''.join(f'{cell:2} ' for cell in show_grid[row])
-            print(f'{row:2} {line}')
-        key = input("enter [q] to quit (or Enter):")
-        if key == 'q':
-            exit()
-
-
+    def display(self, year):
+        try:
+            os.system('cls' if os.name=='nt' else 'clear')
+        except Exception:
+            pass  
+        cols = ''.join(f'{i:<2}' for i in range(1, GRID_SIZE+1))
+        header = f'   {cols} time step = {year}'
+        print(header)
+        print('-' * len(header))
+        for y in range(GRID_SIZE):
+            row = []
+            for x in range(GRID_SIZE):
+                if any(z.x==x and z.y==y for z in self.zebras): row.append('Z ')
+                elif any(l.x==x and l.y==y for l in self.lions): row.append('L ')
+                else: row.append('. ')
+            print(f'{y+1:2}|'+''.join(row)+'|')
+        zs, ls = self.stats()
+        print(f'Zebras: {zs} | Lions: {ls}')
+        try:
+            cmd = input('press Enter to continue, other key to quit: ')
+        except EOFError:
+            cmd = ''
+        if cmd != '':
+            print('Simulation terminated.')
+            sys.exit()
 
 if __name__ == '__main__':
     eco = Ecosystem()
     for year in range(1, YEARS+1):
-        eco.year_pass()
-        zebras, lions = eco.stat()
-        print(f'Year {year:02d}: zebras={zebras}, lions={lions}')
-        # 只显示左上10*10，需全图显示可改area=50
-        eco.display(area=50)
+        eco.step()
+        eco.display(year)
+    print('Simulation complete.')
